42563a4ca9c4d444c452e595035347512a743aef
NehaThakkar25 <nehathakkar3008@gmail.com>
2021-08-25 20:56:16 +0530

    Added Themes Colors

--- js/main.js
+++ js/main.js
@@ -54,6 +54,8 @@
                     })
                     fadeOutEffect();
                 }
+
+                window.location.hash = hash;
             }
         }
         else{
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,1 +1,25 @@
 /* js for theme color switcher */
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})();
+
+window.addEventListener("scroll", () =>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})();
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color){
+    alternateStyles.forEach((style)=>{
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
